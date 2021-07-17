569ebbd896f379aba42b9f348897fd3b91a5459b
Chris Amaya <amayarivasc@gmail.com>
2021-07-17 13:43:31 -0500

    ADD: selection component selection component is the component that will handle the user's selection

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import './App.css';
 import Index from './components/Index';
 import RulesButton from './components/rulesButton/RulesButton'
 import RulesModal from './components/rulesModal/RulesModal';
+import Selection from './components/selection/Selection';
 
 function App() {
 
   const [rulesModal, setRulesModal] = useState(false);
+  const [userSelection, setUserSelection] = useState()
 
-  function toggleRulesDialog() {
-    setRulesModal(!rulesModal)
-  }
+  const toggleRulesDialog = () => setRulesModal(!rulesModal);
 
   return (
     <div className="App">
       <Index />
+      <Selection setUserSelection={setUserSelection} />
       <RulesModal on={rulesModal} setRulesModal={toggleRulesDialog} />
       <RulesButton toggleRulesDialog={toggleRulesDialog} />
     </div>
---
+++ src/components/selection/Selection.js
@@ -0,0 +1,30 @@
+import './Selection.css';
+import { ReactComponent as TriangleSVG } from '../../assets/img/bg-triangle.svg';
+import { ReactComponent as PaperSVG } from '../../assets/img/icon-paper.svg';
+import { ReactComponent as RockSVG } from '../../assets/img/icon-rock.svg';
+import { ReactComponent as ScissorsSVG } from '../../assets/img/icon-scissors.svg';
+
+const Selection = ({setUserSelection}) => {
+
+    return (
+        <div className="selection-container">
+            <div>
+                <TriangleSVG />
+
+                <div className="option paper" onClick={() => setUserSelection('paper')}>
+                    <PaperSVG />
+                </div>
+
+                <div className="option rock" onClick={() => setUserSelection('rock')}>
+                    <RockSVG />
+                </div>
+
+                <div className="option scissors" onClick={() => setUserSelection('scissors')}>
+                    <ScissorsSVG />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Selection;
