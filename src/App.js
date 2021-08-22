ee0a1484f3bdbf28c68a66e41ed08c2ab8398234
Chris Amaya <amayarivasc@gmail.com>
2021-08-22 01:37:45 -0500

    REFACTOR: folder names, code formatted

--- src/App.js
+++ src/App.js
@@ -1,35 +1,65 @@
-import { useState } from 'react';
-import './App.css';
-import Battle from './components/battle/Battle';
-import Menu from './components/menu/Menu';
-import RulesButton from './components/rulesButton/RulesButton'
-import RulesModal from './components/rulesModal/RulesModal';
-import ScoreBoard from './components/scoreBoard/ScoreBoard';
-import UserSelection from './components/userSelection/UserSelection';
+import { useState } from "react";
+import "./App.css";
+import Battle from "./components/Battle/Battle";
+import Menu from "./components/Menu/Menu";
+import RulesButton from "./components/Rules/RulesButton";
+import RulesModal from "./components/Rules/RulesModal";
+import ScoreBoard from "./components/ScoreBoard/ScoreBoard";
+import UserSelection from "./components/UserSelection/UserSelection";
 
 function App() {
-
   const [rulesModal, setRulesModal] = useState(false);
   const [userSelection, setUserSelection] = useState();
   const [score, setScore] = useState(0);
   const [gameType, setGameType] = useState();
   const [modeOption, setModeOption] = useState();
-  const [userName, setUserName] = useState('');
+  const [userName, setUserName] = useState("");
   const [codeRoom, setCodeRoom] = useState();
 
   const toggleRulesDialog = () => setRulesModal(!rulesModal);
   const resetGame = () => {
     setUserSelection();
-  }
+  };
 
   return (
     <div className="App container">
-      <ScoreBoard score={score}  style={{"margin-top": '10px'}} gameType={gameType} />
-      {userSelection && <Battle userSelection={userSelection} score={score} setScore={setScore} resetGame={resetGame} gameType={gameType}/>}
-      {!userSelection && <UserSelection setUserSelection={setUserSelection} gameType={gameType} />}
-      <RulesModal on={rulesModal} setRulesModal={toggleRulesDialog} gameType={gameType} />
+      <ScoreBoard
+        score={score}
+        style={{ "margin-top": "10px" }}
+        gameType={gameType}
+      />
+      {userSelection && (
+        <Battle
+          userSelection={userSelection}
+          score={score}
+          setScore={setScore}
+          resetGame={resetGame}
+          gameType={gameType}
+        />
+      )}
+      {!userSelection && (
+        <UserSelection
+          setUserSelection={setUserSelection}
+          gameType={gameType}
+        />
+      )}
+      <RulesModal
+        on={rulesModal}
+        setRulesModal={toggleRulesDialog}
+        gameType={gameType}
+      />
       <RulesButton toggleRulesDialog={toggleRulesDialog} />
-      {!gameType && <Menu setGameType={setGameType} setModeOption={setModeOption} modeOption={modeOption} setUserName={setUserName} setCodeRoom={setCodeRoom} codeRoom={codeRoom} userName={userName} />}
+      {!gameType && (
+        <Menu
+          setGameType={setGameType}
+          setModeOption={setModeOption}
+          modeOption={modeOption}
+          setUserName={setUserName}
+          setCodeRoom={setCodeRoom}
+          codeRoom={codeRoom}
+          userName={userName}
+        />
+      )}
     </div>
   );
 }
